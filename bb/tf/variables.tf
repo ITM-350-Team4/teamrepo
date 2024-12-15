e49cc0436964223321bb0540b52b2f389b5d52f2
Maddy McGreer <79669704+madelynm3@users.noreply.github.com>
2024-12-14 20:41:54 -0700

    test

--- bb/tf/variables.tf
+++ bb/tf/variables.tf
@@ -1,39 +1,39 @@
 variable "ami" {
-  type = "ami-0e252be8f4dfa2c0d"
+  default = "ami-0e252be8f4dfa2c0d"
 }
 
 variable "cluster_name" {
-  type = "bb-cluster3"
+  default = "bb-cluster3"
 }
 
 variable "container_image" {
-  type = "epacardo/itm350-teamrepo-bb:sha-d42bbfb"
+  default = "epacardo/itm350-teamrepo-bb:sha-d42bbfb"
 }
 
 variable "container_port" {
-  type = 8080
+  default = 8080
 }
 
 variable "instance_name_prefix" {
-  type = "cit262-node"
+  default = "cit262-node"
 }
 
 variable "instance_type" {
-  type = "t2.micro"
+  default = "t2.micro"
 }
 
 variable "lab_role" {
-  type = "arn:aws:iam::353518434405:role/LabRole"
+  default = "arn:aws:iam::353518434405:role/LabRole"
 }
 
 variable "region" {
-  type = "us-west-2"
+  default = "us-west-2"
 }
 
 variable "vpc_cidr" {
-  type = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "vpc_prefix" {
-  type = "ecs-vpc"
+  default = "ecs-vpc"
 }
